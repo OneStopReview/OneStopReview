63f731f0800097ba2a6776194ecc17bb3c03b346
Rakesh <rparuthikkat@steris.com>
2017-04-10 23:15:09 -0400

    Add LoginActivity.

--- app/src/main/java/onestopreview/codepath/onestopreview/activities/MainActivity.java
+++ app/src/main/java/onestopreview/codepath/onestopreview/activities/MainActivity.java
@@ -2,8 +2,6 @@ package onestopreview.codepath.onestopreview.activities;
 
 import android.os.Bundle;
 
-import com.parse.ui.ParseLoginBuilder;
-
 import onestopreview.codepath.onestopreview.R;
 
 public class MainActivity extends BaseActivity {
@@ -13,7 +11,6 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
-        startActivityForResult(builder.build(), 0);
+
     }
 }
